e8fc077fcf68daa451d8a17de026767e1c43809d
Helga998 <bom1993@yandex.ru>
2024-04-06 13:50:11 +0300

    Added solve function

--- sudoku.js
+++ sudoku.js
@@ -1,14 +1,52 @@
-function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
+function read(match) {
+  const fs = require("fs");
+  const { EOL } = require("os");
+  const puzzlesTxt = fs.readFileSync("./puzzles.txt", "utf-8");
+
+  //arrPuzzles - одномерный массивб каждый элемент это строка из puzzle.txt
+  const arrPuzzles = puzzlesTxt.split(EOL);
+
+  //arrSudoku - массив где каждый элемент это одно значение всего судоку (81 элемент)
+  let arrSudoku;
+  for (let i = 0; i <= arrPuzzles.length; i += 1) {
+    arrSudoku = arrPuzzles[match - 1].split("");
+    break;
+  }
+
+  //playngField - игровое поле, двумерный массив, где вложенные масивы это строки по 9 элементов
+  let playingField = [];
+  for (let i = 0; i < arrSudoku.length; i += 9) {
+    playingField.push(arrSudoku.slice(i, i + 9));
+  }
+  return playingField;
 }
 
 function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+  const arr = read(num);
+  const askArrSolved = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    let etalon = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    askArrSolved[i] = arr[i]
+      .map((el) => {
+        if (el !== "-") {
+          etalon.splice(etalon.indexOf(el), 1);
+          return el;
+        } else {
+          return el;
+        }
+      })
+      .map((el) => {
+        if (el === "-") {
+          let index = Math.floor(Math.random() * etalon.length);
+          el = etalon[index];
+          etalon.splice(index, 1);
+          return el;
+        } else {
+          return el;
+        }
+      });
+  }
+  return askArrSolved;
 }
 
 function isSolved() {
